7e17b898c0c0861b76b0b80ed16ed9749333d388
Laurel Herting <laurel.herting@gmail.com>
2017-08-21 19:29:19 -0600

    add style, test, file changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 import React, { Component } from 'react';
-import './index.scss';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom';
+import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        Test
+      <Router>
+        <div>
+          <div className="App">
+            <div className="App-header">
+              <div>
+                <a href="http://wallpapercave.com/w/pBcuEih"><img src="http://wallpapercave.com/wp/pBcuEih.jpg" width="700" height="120" /></a>
+              </div>
+              <h2>Harmon Software Solutions</h2>
+            </div>
+        </div>
+
+        <nav>
+          <Link to="/">Home</Link>
+        </nav>
       </div>
+      </Router>
     );
   }
 }
