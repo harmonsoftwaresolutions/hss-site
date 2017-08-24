a24847be383f1e33eaf35c4a07505a733b4eaa6a
Evan Harmon <evan.p.harmon@gmail.com>
2017-08-24 09:00:34 -0700

    resolve conflicts

--- src/index.js
+++ src/index.js
@@ -1,18 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-<<<<<<< HEAD
-import App from './App';
-import './index.css';
-
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
-=======
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
->>>>>>> clear all files and restart with a new create-react-app
