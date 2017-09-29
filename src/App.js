aceb8152343827b5f96f8925fcb668fae4da2d14
Evan Harmon <evan.p.harmon@gmail.com>
2017-09-28 21:14:10 -0700

    refactor(big): prettier and latest react-app

--- src/App.js
+++ src/App.js
@@ -1,45 +1,37 @@
-import React, { Component } from 'react';
-import {
-  BrowserRouter as Router,
-  Link
-} from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter as Router, Link } from 'react-router-dom';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <Router>
+const App = () => (
+  <Router>
+    <div>
+      <div className="App">
+        <h2>Harmon Software Solutions</h2>
         <div>
-          <div className="App">
-            <h2>Harmon Software Solutions</h2>
-          <div>
-            <section>
+          <section>
             <div className="">
               <div className="sectiontitle grid-item">Amazon Web Services</div>
-              
-              <div className="sectiondescription grid-item">description</div> 
+
+              <div className="sectiondescription grid-item">description</div>
             </div>
-            </section>
+          </section>
 
-            <section>
-              <div className="sectiontitle grid-item">Microservices</div>
-              <div className="sectiondescription grid-item">description</div>
-            </section>
+          <section>
+            <div className="sectiontitle grid-item">Microservices</div>
+            <div className="sectiondescription grid-item">description</div>
+          </section>
 
-            <section>
-              <div className="sectiontitle grid-item">React / Node</div>
-              <div className="sectiondescription grid-item">description</div>
-            </section>
-
-          </div>
-          <nav>
-            <Link to="/">Contact</Link>
-          </nav>
+          <section>
+            <div className="sectiontitle grid-item">React / Node</div>
+            <div className="sectiondescription grid-item">description</div>
+          </section>
         </div>
+        <nav>
+          <Link to="/">Contact</Link>
+        </nav>
       </div>
-    </Router>
-    );
-  }
-}
+    </div>
+  </Router>
+);
 
 export default App;
