d767aaa2856b45e531678ce52f1fa8aa743fcd5e
Evan Harmon <evan.p.harmon@gmail.com>
2017-10-06 21:26:16 -0700

    feat(fa): adjust fa em size and fix Footer.js git ref

--- src/Footer.js
+++ src/Footer.js
@@ -5,7 +5,6 @@ const Footer = () => (
     <div className="footer-item">
       <a href="https://github.com/harmonsoftwaresolutions">
         <i className="fa fa-github" aria-hidden="true" />
-        Github logo goes here
       </a>
     </div>
   </div>
