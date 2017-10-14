8d4fce2408e2ec9b5835a4ac4196f443072a65ce
Laurel Herting <laurel.herting@gmail.com>
2017-10-14 13:32:41 -0600

    Updates

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'font-awesome/css/font-awesome.css';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
